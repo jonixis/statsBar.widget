650c1e48e3a8501498bc3670367a4cf58bf6a729
Jonathan Stahl <jonathan.stahl@uzh.ch>
2019-06-12 10:59:35 +0200

    fix wifi script

--- scripts/wifi.sh
+++ scripts/wifi.sh
@@ -3,23 +3,22 @@
 services=$(networksetup -listnetworkserviceorder | grep 'Hardware Port')
 
 while read line; do
-    sname=$(echo $line | awk -F  "(, )|(: )|[)]" '{print $2}')
-    sdev=$(echo $line | awk -F  "(, )|(: )|[)]" '{print $4}')
+    sname=$(echo "$line" | awk -F  "(, )|(: )|[)]" '{print $2}')
+    sdev=$(echo "$line" | awk -F  "(, )|(: )|[)]" '{print $4}')
     # echo "Current service: $sname, $sdev, $currentservice"
     if [ -n "$sdev" ]; then
-        ifconfig $sdev 2>/dev/null | grep 'status: active' > /dev/null 2>&1
+        ifconfig "$sdev" 2>/dev/null | grep 'status: active' > /dev/null 2>&1
         rc="$?"
         if [ "$rc" -eq 0 ]; then
-            currentservice="$sname"
-	    currentsdev="$sdev"
-            break
+            networkName=$(networksetup -getairportnetwork "$sdev")
+            [ $? -ne 0 ] && continue
+            result="$sname@$(cut -c 24- <<< $networkName)@$(networksetup -getinfo Wi-Fi | grep "IP address" | grep "\." | cut -c 13-)"
+            if [ -n "$result" ]; then
+                echo "$result";
+                exit 0;
+            fi
         fi
     fi
-done <<< "$(echo "$services")"
+done <<< "$services"
 
-if [ -n "$currentservice" ] ; then
-    echo "$currentservice@$(networksetup -getairportnetwork $currentsdev | cut -c 24-)@$(networksetup -getinfo Wi-Fi | grep "IP address" | grep "\." | cut -c 13-)"
-else
-    >&1 echo "none@none@"
-    exit 1
-fi
+echo "--@--@--"
